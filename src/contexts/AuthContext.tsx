b5fcada00ed1caaddd0d9bd3477980f7bb33dd77
Verdant31 <joao.pedro3110@hotmail.com>
2022-10-16 22:51:34 -0300

    WIP

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,13 +1,20 @@
-import { createContext, ReactNode, useContext, useState } from 'react'
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
 import { User } from '../utils/types'
 import { useRouter } from 'next/router'
 import { api } from '../services/axios'
-import axios from 'axios'
+import { destroyCookie, parseCookies, setCookie } from 'nookies'
 
 type AuthContextType = {
   user: User | undefined
-  logIn: (matricula: string, senha: string) => Promise<string>
+  logIn: (matricula: string, senha: string) => void
   logOut: () => void
+  isAuthenticated: boolean
 }
 
 type AuthContextProviderPros = {
@@ -18,19 +25,53 @@ export const AuthContext = createContext({} as AuthContextType)
 
 const AuthContextProvider = (props: AuthContextProviderPros) => {
   const [user, setUser] = useState<User>()
+  const isAuthenticated = !!user
   const router = useRouter()
 
-  const logIn = async (matricula: string, senha: string) => {
-    const temporaryName = 'Haissam'
+  useEffect(() => {
+    const { 'monitoria.token': token } = parseCookies()
+    if (token) {
+      console.log('Tem token po')
+      api
+        .get('/auth/me')
+        .then((res) => {
+          const { role, matricula, nome } = res.data
+          setUser({ matricula, role, nome })
+          if (role === 'Coordenador') {
+            router.push('/DashCord')
+          } else {
+            router.push('/DashProf')
+          }
+        })
+        .catch((err) => {
+          destroyCookie(undefined, 'monitoria.token')
+          destroyCookie(undefined, 'monitoria.refreshToken')
+          console.log(err)
+          logOut()
+        })
+    }
+  }, [])
 
-    return await api
+  const logIn = async (matricula: string, senha: string) => {
+    await api
       .post('/auth/login/colaborador', { matricula, senha })
       .then((res) => {
-        const { role, token } = res.data
-        localStorage.setItem('token', token)
-        axios.defaults.headers.common = { Authorization: token }
-        setUser({ matricula, role, token, nome: temporaryName })
-        return role
+        const { role, token, matricula, refreshToken, nome } = res.data
+        setUser({ matricula, role, nome })
+        setCookie(undefined, 'monitoria.token', token, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/',
+        })
+        setCookie(undefined, 'monitoria.refreshToken', refreshToken, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/',
+        })
+        api.defaults.headers.common.Authorization = `${token}`
+        if (role === 'Coordenador') {
+          router.push('/DashCord')
+        } else {
+          router.push('/DashProf')
+        }
       })
   }
   const logOut = () => {
@@ -39,7 +80,7 @@ const AuthContextProvider = (props: AuthContextProviderPros) => {
   }
 
   return (
-    <AuthContext.Provider value={{ user, logIn, logOut }}>
+    <AuthContext.Provider value={{ isAuthenticated, user, logIn, logOut }}>
       {props.children}
     </AuthContext.Provider>
   )
--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,10 +1,76 @@
-import axios from 'axios'
-
-if (typeof window !== 'undefined') {
-  const token = localStorage.getItem('token')
-  if (token) axios.defaults.headers.common = { Authorization: token }
-}
-
+import axios, { AxiosError } from 'axios'
+import { parseCookies, setCookie } from 'nookies'
+let cookies = parseCookies()
+let isRefreshing = false
+let failedRequestsQueue: any = []
 export const api = axios.create({
-  baseURL: 'https://projeto-monitoria-api.herokuapp.com',
+  baseURL: 'http://localhost:3001',
 })
+
+api.defaults.headers.common.Authorization = `${cookies['monitoria.token']}`
+api.interceptors.response.use(
+  (res) => {
+    return res
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      if (error.response.data?.code === 'token.expired') {
+        cookies = parseCookies()
+        const { 'monitoria.refreshToken': refreshToken } = cookies
+        const orinalConfig = error.config
+
+        if (!isRefreshing) {
+          isRefreshing = true
+
+          api
+            .post('/refresh', { refreshToken })
+            .then((res) => {
+              const { token } = res.data
+              setCookie(undefined, 'monitoria.token', token, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/',
+              })
+              setCookie(
+                undefined,
+                'monitoria.refreshToken',
+                res.data.refreshToken,
+                {
+                  maxAge: 60 * 60 * 24 * 30, // 30 days
+                  path: '/',
+                },
+              )
+
+              api.defaults.headers.common.Authorization = `${cookies['monitoria.token']}`
+              failedRequestsQueue.forEach((request: any) =>
+                request.onSuccess(token),
+              )
+              failedRequestsQueue = []
+            })
+            .catch((err) => {
+              failedRequestsQueue.forEach((request: any) =>
+                request.onFailure(err),
+              )
+              failedRequestsQueue = []
+            })
+            .finally(() => {
+              isRefreshing = false
+            })
+        }
+
+        return new Promise((resolve, reject) => {
+          failedRequestsQueue.push({
+            onSuccess: (token: string) => {
+              orinalConfig.headers.Authorization = `Bearer ${token}`
+              resolve(api(orinalConfig))
+            },
+            onFailure: (err: AxiosError) => {
+              reject(err)
+            },
+          })
+        })
+      } else {
+        console.log('Deslogar o usuario')
+      }
+    }
+  },
+)
